5ea8117bde5f18f7c732402b55f7cf1a77d2fc2c
Kendall Arneaud <kendall.arneaud@gmail.com>
2023-09-11 11:50:38 -0400

    Update postCreateCommand.sh
    do i need sudo?

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -8,17 +8,17 @@ fi
 
 # Start Mysql
 echo "Setup MYSQL..."
-sudo service mysql start 
-sudo mysqladmin create $MYSQL_DATABASE
-sudo mysqladmin create wordpress
-sudo mysqladmin --host=localhost password $MYSQL_USER_PASSWORD
-sudo mysql -hlocalhost -uroot -p$MYSQL_USER_PASSWORD -e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_USER_PASSWORD'; GRANT ALL PRIVILEGES ON wordpress.* TO '$MYSQL_USER'@'localhost'; GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost'; GRANT ALL PRIVILEGES ON wordpress.* TO '$MYSQL_USER'@'localhost'; FLUSH PRIVILEGES;"
+service mysql start 
+mysqladmin create $MYSQL_DATABASE
+mysqladmin create wordpress
+mysqladmin --host=localhost password $MYSQL_USER_PASSWORD
+mysql -hlocalhost -uroot -p$MYSQL_USER_PASSWORD -e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_USER_PASSWORD'; GRANT ALL PRIVILEGES ON wordpress.* TO '$MYSQL_USER'@'localhost'; GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost'; GRANT ALL PRIVILEGES ON wordpress.* TO '$MYSQL_USER'@'localhost'; FLUSH PRIVILEGES;"
 
 # Apache
-sudo chmod 777 /etc/apache2/sites-available/000-default.conf
-sudo sed "s@.*DocumentRoot.*@\tDocumentRoot $PWD/wordpress@" .devcontainer/000-default.conf > /etc/apache2/sites-available/000-default.conf
+chmod 777 /etc/apache2/sites-available/000-default.conf
+sed "s@.*DocumentRoot.*@\tDocumentRoot $PWD/wordpress@" .devcontainer/000-default.conf > /etc/apache2/sites-available/000-default.conf
 update-rc.d apache2 defaults 
-sudo service apache2 restart
+service apache2 restart
 
 LOCALE="en_US"
 
